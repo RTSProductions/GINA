f3205807329ba2fcfe9fd12b5815cfa0da4e2bf0
Regin Hurley David <reginhurleydavid@MacBook-Air-2.local>
2023-06-11 23:50:52 -0600

    Trees

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
 
-#define MAX_COLUMNS 20
+// Scene drawing
+void DrawScene(void)
+{
+    int count = 5;
+    float spacing = 4;
+
+    // Grid of cube trees on a plane to make a "world"
+    DrawPlane((Vector3){ 0, 0, 0 }, (Vector2){ 50, 50 }, BEIGE); // Simple world plane
+
+    for (float x = -count*spacing; x <= count*spacing; x += spacing)
+    {
+        for (float z = -count*spacing; z <= count*spacing; z += spacing)
+        {
+            DrawCube((Vector3) { x, 1.5f, z }, 1, 1, 1, LIME);
+            DrawCube((Vector3) { x, 0.5f, z }, 0.25f, 1, 0.25f, BROWN);
+        }
+    }
+}
 
 int main(void)
 {
@@ -14,25 +31,13 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "GINA");
 
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 2.0f, 10.0f };  // Camera position
+    camera.position = (Vector3){ 0.0f, 1.0f, 10.0f };  // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 60.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
     int cameraMode = CAMERA_FIRST_PERSON;
-
-    // Generates some random columns
-    float heights[MAX_COLUMNS] = { 0 };
-    Vector3 positions[MAX_COLUMNS] = { 0 };
-    Color colors[MAX_COLUMNS] = { 0 };
-
-    for (int i = 0; i < MAX_COLUMNS; i++)
-    {
-        heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-        colors[i] = (Color){255, 55, 30, 255 };
-    }
 
     DisableCursor();                    // Limit cursor to relative movement inside the window
 
@@ -63,41 +68,31 @@ int main(void)
         //Draw
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(SKYBLUE);
 
            BeginMode3D(camera);
 
-                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-                DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
-                DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
-                DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
-
-                // Draw some cubes around
-                for (int i = 0; i < MAX_COLUMNS; i++)
-                {
-                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-                }
+           DrawScene();
 
             EndMode3D();
 
             // Draw info boxes
-            DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(5, 5, 330, 100, BLUE);
+            // DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
+            // DrawRectangleLines(5, 5, 330, 100, BLUE);
 
-            DrawText("Camera controls:", 15, 15, 10, BLACK);
-            DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
-            DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
-            DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
-            DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
-            DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
+            // DrawText("Camera controls:", 15, 15, 10, BLACK);
+            // DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+            // DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+            // DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
+            // DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
+            // DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
 
-            DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(600, 5, 195, 100, BLUE);
+            // DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
+            // DrawRectangleLines(600, 5, 195, 100, BLUE);
 
-            DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
-            DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
-            DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
+            // DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
+            // DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
+            // DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
 
         EndDrawing();
     }
